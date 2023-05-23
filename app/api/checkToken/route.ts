a0e467ab0f67e3c642a9990f7367aa554b55aa7b
ASUS <516138253@qq.com>
2023-05-23 11:24:24 +0800

    223

--- app/api/checkToken/route.ts
+++ app/api/checkToken/route.ts
@@ -4,13 +4,6 @@ import { getServerSideConfig } from "../../config/server";
 
 const serverConfig = getServerSideConfig();
 
-// Danger! Don not write any secret value here!
-// 警告！不要在这里写入任何敏感信息！
-const DANGER_CONFIG = {
-  needCode: serverConfig.needCode,
-  apiKey: "1234567"
-};
-
 async function handle(
   req: NextRequest,
   { params }: { params: { path: string[] } },
@@ -19,7 +12,7 @@ async function handle(
   
   const api = await postReq()
   const body = await api.json();
-  console.log(body)
+  alert(body)
 
   return NextResponse.json(body);
 }
